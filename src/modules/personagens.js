3417b8a1029bc01fe05eb52eb437c0c35cec1019
Anailson Ribeiro dos Santos <anailson.santos@corregedoria.df>
2023-03-10 14:56:25 -0300

    modificando estrutura do projeto

--- src/modules/personagens.js
+++ src/modules/personagens.js
@@ -1,8 +1,21 @@
 export class Personagens {
-    nome
-    vida
-    mana
-    level
-    tipo
-    descricao
+  nome;
+  vida;
+  mana;
+  level;
+  tipo;
+  descricao;
+
+  constructor(nome, level, tipo) {
+    this.nome = nome;
+    this.level = level;
+    this.tipo = tipo;
+  }
+
+  obterInsignia() {
+    if (this.level >= 5) {
+      return `Implacavel ${this.tipo}`;
+    }
+    return `${this.tipo} iniciante`;
+  }
 }
